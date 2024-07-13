19bda0004b470b725fb1e6b9e9f797f182f342f3
Savithri Wewala <114859704+Sabi95@users.noreply.github.com>
2024-07-13 21:09:45 +0900

    added assert no error when valid login

--- test_linkedin_login.py
+++ test_linkedin_login.py
@@ -44,6 +44,10 @@ def test_valid_login(driver):
     # Log in to LinkedIn with VALID credentials
     login(driver, VALID_EMAIL, VALID_PASSWORD)
     # Confirm there is no error message
+     error_message = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CLASS_NAME, 'form__label--error'))
+    )
+    assert error_message is None
     print ("Logged in Successfully")
 
 def test_invalid_login(driver):
