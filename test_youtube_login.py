0700576abe7a45f405d80f4acf99b87a7988951b
Savithri Wewala <s.w.wewala@gmail.com>
2024-07-13 15:23:36 +0900

    added credentials

--- test_youtube_login.py
+++ test_youtube_login.py
@@ -5,3 +5,8 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
+
+# Youtube credentials
+VALID_EMAIL = 't70323534@gmail.com'
+VALID_PASSWORD = 'test123'
+INVALID_PASSWORD = 'invalid123'
