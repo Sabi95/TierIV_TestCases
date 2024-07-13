027897ecb04962824f6a987da79915ddd169e042
Savithri Wewala <s.w.wewala@gmail.com>
2024-07-13 17:42:42 +0900

    changed to linked in

--- test_youtube_login.py
+++ test_youtube_login.py
@@ -6,9 +6,10 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 
-# Youtube credentials
+# LinkedIn credentials
 VALID_EMAIL = 't70323534@gmail.com'
-VALID_PASSWORD = 'test1234!'
+VALID_PASSWORD = 'test123'
+INVALID_EMAIL = 'invalidemail@test.com'
 INVALID_PASSWORD = 'invalid123'
 
 
@@ -19,6 +20,25 @@ def driver():
     yield driver
     # Quit driver after test
     driver.quit()
+
+def login(driver, email, password):
+    # Open LinkedIn login page
+    driver.get('https://www.linkedin.com/login')
+    # Identify email field and enter email
+    email_field = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, 'username'))
+    )
+    email_field.send_keys(email)
+    # Identify password field and enter password
+    password_field = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, 'password'))
+    )
+    password_field.send_keys(password)
+    # Identify login button and click 'Sign In' button
+    login_button = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, '//*[@type="submit"]'))
+    )
+    login_button.click()
 
 def test_valid_login(driver):
     # Open Youtube page
